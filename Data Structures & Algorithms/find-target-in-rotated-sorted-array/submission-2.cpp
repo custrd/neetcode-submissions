4f82efd60b0e1dad91147d4643c377d13e1a8b73
Kartikey <121079200+custrd@users.noreply.github.com>
2026-08-27 23:43:14 +0530

    Add: find-target-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int lo = 0, hi = n-1;
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<=nums[hi]){
+                if(target > nums[mid]){
+                    if(target > nums[hi]) hi = mid-1;
+                    else lo = mid+1;
+                }else{
+                    hi = mid-1;
+                }
+            }else{
+                if(nums[lo] <= target && target <nums[mid]) hi=mid-1;
+                else lo = mid+1;
+            }
+        }
+        return -1;
+    }
+};
