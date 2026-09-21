5ae550e5adcc65ef63030d3112ae9aaa4a062b2b
Kartikey <121079200+custrd@users.noreply.github.com>
2026-09-21 15:04:32 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,15 @@
+int solve(int n, vector<int> &dp){
+    if(n<=2) return n;
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+}
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
+    }
+};
